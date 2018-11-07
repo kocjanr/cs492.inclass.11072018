be0436326acc6393716da7ec4956a58c338a8e73
kocjanr <kocjanr@onid.oregonstate.edu>
2018-11-07 11:48:04 -0800

    return all data

--- app/src/main/java/com/kocjan/kocjanryaninclass/DatabaseHelper.java
+++ app/src/main/java/com/kocjan/kocjanryaninclass/DatabaseHelper.java
@@ -2,6 +2,7 @@ package com.kocjan.kocjanryaninclass;
 
 import android.content.ContentValues;
 import android.content.Context;
+import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
 
@@ -45,5 +46,11 @@ public class DatabaseHelper extends SQLiteOpenHelper {
             return false;
         } else
             return true;
+    }
+
+    public Cursor getAllData(){
+        SQLiteDatabase db = this.getWritableDatabase();
+        Cursor cursor = db.rawQuery("SELECT * FROM " + NAME, null);
+        return cursor;
     }
 }
